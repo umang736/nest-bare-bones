4b5079e72be3560ddd694b660ee002edb6c43c82
umang736 <umanggupta131995@gmail.com>
2022-07-20 18:58:06 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -10,19 +10,10 @@ async function bootstrap() {
     'https://angular-ivy-eajmf8.stackblitz.io',
   ];
   app.enableCors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        console.log('allowed cors for:', origin);
-        callback(null, true);
-      } else {
-        console.log('blocked cors for:', origin);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: '*',
     allowedHeaders:
       'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
     methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-    credentials: true,
   });
   await app.listen(3000);
 }
